62407077fed0bce3504f0193f9c01630a7e94f93
matin <you@example.commatinlotfi1994@gmail.com config --global user.name Your>
2025-09-17 16:33:20 +0330

    this is figma

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./blog.html", "./db.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      mobile: { max: "999px" },
+      desktop: { min: "1000px" },
+    },
+  },
+  plugins: [],
+};
+
+
